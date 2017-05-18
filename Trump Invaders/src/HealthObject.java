700c3fd891ca9e7fafe1b6bda96c1f2f5336de9c
Greg Leitkowski <greg_s_l@sbcglobal.net>
2017-05-18 10:39:30 -0400

    second constructor

--- Trump Invaders/src/HealthObject.java
+++ Trump Invaders/src/HealthObject.java
@@ -2,11 +2,14 @@ import java.awt.Image;
 
 public class HealthObject extends Sprite{
     private int health;
-    private boolean isAlive;
+    private boolean isAlive = true;
 
     public HealthObject(Image image){
         super(image);
         health = 100;
-        isAlive = true;
+    }
+    public HealthObject(Image image, int health){
+        super(image);
+        this.health = health;
     }
 }
