401c615f5264d8b1f0b4cfecc9df8b4248ef13dd
Will Anderson <wllmscttandrsn@gmail.com>
2017-05-22 10:21:54 -0400

    ???

--- Trump Invaders/src/Trump.java
+++ Trump Invaders/src/Trump.java
@@ -1,13 +1,16 @@
+import java.awt.*;
+
 /**
  * Created by mhall17162 on 5/12/2017.
  */
-public class Trump extends HealthObject {
+public class Trump extends Sprite {
+    private static Image trumpFace;
 
     public Trump(){
-        super(//trumpimage);
+        super(trumpFace);
     }
 
     public void playSound(){
-        
+
     }
 }
